fc3c1422a0a7218abfa8792f75229dc0325dec7e
victorejike <victorejike@gmail.com>
2026-07-21 11:08:16 +0100

    python output option

---
+++ week1/helo.py
@@ -0,0 +1,21 @@
+x = "hello"
+y = 2.348
+z = 2
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x= y= z = "victor"
+print(x,y,z)
+
+#this is unpacking in python
+victor = ["hello", "world", "how"]
+x, y, z = victor
+print(x,y,z)
+
+#output variable in python
+x = "python3 "
+y = "is "
+z = "awesome "
+print(x+y+z)
