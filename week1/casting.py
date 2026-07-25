1e975a80ed805cbbf3ddf372dd1ae69469a94df5
victorejike <victorejiki@gmail.com>
2026-07-25 18:50:50 +0100

    casting in python

---
+++ week1/casting.py
@@ -0,0 +1,11 @@
+#Create a variable x with the integer value 1
+#Convert x to a float and store it in a
+#Convert x to a string and store it in b
+#Print a and b
+
+x = 1
+
+a = float(x)
+
+b = str(x)
+print(a,b)
