8070fe955a5ee9c7c559530a2292e515ae625e66
victorejike <victorejiki@gmail.com>
2026-07-30 18:26:42 +0100

    multpy string

--- week1/string.py
+++ week1/string.py
@@ -2,3 +2,12 @@
 
 a = '''this is a multup stringss 
 with'''
+print(a[2])
+print(len(a))
+
+txt = "hello world what"
+print("hello" in txt)
+
+test = "lis of array in a block of code"
+if 'array' in test:
+    print("yes, 'test' found!  ", test)
