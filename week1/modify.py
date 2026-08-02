6f890da469258d561c16d7ad39e742ebe4951cf3
victorejike <victorejiki@gmail.com>
2026-08-02 23:45:13 +0100

    localhost

--- week1/modify.py
+++ week1/modify.py
@@ -1,4 +1,22 @@
-#this is use for string manpulation in python
-txt = "helo world"
-print(txt.replace("h", "i"))
-# this changes text  the first old one and replace it the new one
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Router(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            self.wfile.write(bytes("Hello, Welcome to the home page!", "utf-8"))
+        
+def main():
+    address = ("localhost", 8080)
+    server = HTTPServer(address, Router)
+    try:
+        print("Starting")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nStopped")
+        sever.server_close()
+if __name__ == "__main__":
+    main(/)
