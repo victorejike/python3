9fda98225569a794fc1f88db6d50a85b226e23b8
victor ejike <victorejiki@gmail.com>
2026-08-29 21:37:15 +0100

    dixtionary

--- week1/dist.py
+++ week1/dist.py
@@ -7,4 +7,18 @@ def dictionary ():
 
 dictionary()
 
-    
+car = {
+    "brand1": "toyota",
+    "brand": "mercadice-banz",
+    "year": 2024
+}
+
+print(car)
+
+x = car
+
+print(x)
+
+car["brand1"] = "lambogein"
+
+print(x)
