00ee73a159daaf93798907f9ab369b897e368eb9
victor ejike <victorejiki@gmail.com>
2026-08-31 23:04:44 +0100

    hello

---
+++ week1/diction.py
@@ -0,0 +1,10 @@
+thisdist = {
+    "model": "toyota",
+    "house" : " duplex",
+    "user": "victor",
+    "password": "victor1"
+    
+}
+
+if "model" in thisdist:
+    print(" yes' model' is one of the key in the thisdict dictionary")
