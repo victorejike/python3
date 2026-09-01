4758864bc4093c8282ab384e9b4b7a8539ea7c3d
victor ejike <victorejiki@gmail.com>
2026-09-01 22:01:48 +0100

    code

---
+++ week1/nesteddict.py
@@ -0,0 +1,19 @@
+myfamily = {
+    "child1" = {
+        "house" : "room106",
+        "name" : "victor",
+        "contact" : "0806246346777",
+    },
+    "child2" = {
+        "house": "room206",
+        "name": " ejike",
+        "contact": "07045216655",
+    },
+    "child3" = {
+        "name" : "linus",
+        " year": 5000
+    },
+    
+}
+
+print(myfamily)
